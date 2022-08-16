6adfbc4bc1d75834e50b1a863e10c80b7448287a
Łukasz Wychowaniec <lukasz.wychowaniec@kontomatik.com>
2022-08-16 17:47:02 +0200

    good enough

--- .github/workflows/bad_commit_message_blocker.py
+++ .github/workflows/bad_commit_message_blocker.py
@@ -1,5 +1,9 @@
 import sys
 
-print("Received commits message:" + sys.argv[2])
+message = sys.argv[1]
+print("Received commits message:" + message)
 print()
+if len(message) < 7:
+    print("Message too short: " + message)
+    sys.exit(1)
 sys.exit(0)
--- .github/workflows/entry.sh
+++ .github/workflows/entry.sh
@@ -11,5 +11,5 @@ commits_since_master=$(git rev-list HEAD ^origin/main)
 while read -r commit_hash; do
     commit_message="$(git log --format=%B -n 1 ${commit_hash})"
     python3 bad_commit_message_blocker.py \
-        --message "${commit_message}"
+        "${commit_message}"
 done <<< "$commits_since_master"
