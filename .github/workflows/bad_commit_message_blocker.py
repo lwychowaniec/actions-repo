fb6c8f10ae9948c621a3aeebac3bd591c9eb68b7
Łukasz Wychowaniec <lukasz.wychowaniec@kontomatik.com>
2022-08-17 15:57:26 +0200

    wip script

--- .github/workflows/bad_commit_message_blocker.py
+++ .github/workflows/bad_commit_message_blocker.py
@@ -1,9 +1,38 @@
 import sys
+from curses.ascii import isupper
 
-message = sys.argv[1]
-print("Received commits message:" + message)
-print()
-if len(message) < 7:
-    print("Message too short: " + message)
-    sys.exit(1)
-sys.exit(0)
+
+def contains_review_leftover(message: str) -> bool:
+    lowercase = message.lower()
+    words = lowercase.split()
+    for word in words:
+        if (word == 'cr') | (word == 'wip'):
+            print('Detected review leftover in commit message')
+            return True
+
+
+def is_suspiciously_short(message: str) -> bool:
+    if len(message) <= 7:
+        print("Suspiciously short commit message")
+        return True
+    return False
+
+
+def doesnt_start_with_capital_letter(message: str) -> bool:
+    if ':' in message:
+        index_after_colon = message.index(':') + 1
+        after_tags = message[index_after_colon:]
+        return (after_tags[0] == ' ') & (isupper(after_tags[1]))
+    else:
+        return isupper(message[0])
+
+
+def main():
+    message = sys.argv[1]
+    if contains_review_leftover(message) | is_suspiciously_short(message) | doesnt_start_with_capital_letter(message):
+        sys.exit(1)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
---
+++ .github/workflows/checker_test.py
@@ -0,0 +1,41 @@
+import unittest
+import bad_commit_message_blocker as checker
+
+
+class TestCommitChecker(unittest.TestCase):
+
+    def test_detects_review_leftovers(self):
+        test_messages = ['cr', 'Cr', 'poprawki po CR', 'wip fancy functionality', 'WIP']
+        for message in test_messages:
+            with self.subTest():
+                self.assertTrue(checker.contains_review_leftover(message))
+
+    def test_cr_and_wip_are_fine_as_part_of_word(self):
+        test_messages = ['Add crone', 'CrockAndRoll', 'nowipSense', 'wiproom']
+        for message in test_messages:
+            with self.subTest():
+                self.assertFalse(checker.contains_review_leftover(message))
+
+    def test_detects_too_short_messages(self):
+        message = 'short'
+        self.assertTrue(checker.is_suspiciously_short(message))
+
+    def test_title_does_not_start_with_capital_letter(self):
+        test_messages = ['API-6666: delete all tests',
+                         'API-6667:fix sth',
+                         'no tags here',
+                         'API-5555:No space after colon']
+        for message in test_messages:
+            with self.subTest():
+                self.assertFalse(checker.doesnt_start_with_capital_letter(message))
+
+    def test_title_starts_with_capital_letter(self):
+        test_messages = ['API-6666: Im fine',
+                         'No tags here']
+        for message in test_messages:
+            with self.subTest():
+                self.assertTrue(checker.doesnt_start_with_capital_letter(message))
+
+
+if __name__ == '__main__':
+    unittest.main()
