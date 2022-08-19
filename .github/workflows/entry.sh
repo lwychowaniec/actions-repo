1bd361bf537ee5229e3b0caae6ceb6d41d4b7a21
Łukasz Wychowaniec <lukasz.wychowaniec@kontomatik.com>
2022-08-19 13:19:16 +0200

    Try not fetching main

--- .github/workflows/entry.sh
+++ .github/workflows/entry.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -eu
 
-eval git fetch origin ${GITHUB_HEAD_REF}
-eval git fetch origin main
-
+eval git fetch
 script_dir="$(dirname "$0")"
 cd $script_dir
-eval git checkout ${GITHUB_HEAD_REF}
-commits_since_master=$(git rev-list HEAD ^origin/main)
+commits_since_master=$(git rev-list HEAD ^main)
 
 while read -r commit_hash; do
     commit_message="$(git log --format=%B -n 1 ${commit_hash})"
