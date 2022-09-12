c90dc2b70aa9fd850cd6b79dfc2ad037516be565
Łukasz Wychowaniec <lukasz.wychowaniec@kontomatik.com>
2022-09-12 14:23:06 +0200

    echo commit

--- .github/workflows/entry.sh
+++ .github/workflows/entry.sh
@@ -7,5 +7,6 @@ commits_since_master=$(git rev-list HEAD ^origin/main)
 
 while read -r commit_hash; do
     commit_message="$(git log --format=%B -n 1 ${commit_hash})"
+    echo $commit_message
     python3 bad_commit_message_blocker.py "${commit_message}"
 done <<< "$commits_since_master"
